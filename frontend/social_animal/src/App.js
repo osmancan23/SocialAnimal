bd2e646dea8f8b3ae20e2ed84e4e731156358856
Osmancan Akagündüz <83430594+osmancan23@users.noreply.github.com>
2022-10-16 21:20:51 +0300

    added react file

--- frontend/social_animal/src/App.js
+++ frontend/social_animal/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import logo from "./images/cat_logo.jpg"; // with import
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+
+        <div class="logo">
+            <img src={logo} alt="Logo"/>
+        </div>
+
+        <form action="#">
+
+            <div class="title">KAYIT EKRANI</div>
+
+            <div class="input-box">
+                <input type="email" placeholder="Adınızı Giriniz..." required/>
+                <div class="underline"></div>
+            </div>
+
+            <div class="input-box">
+                <input type="password" placeholder="Soyadınızı Giriniz..." required/>
+                <div class="underline"></div>
+            </div>
+
+            <div class="input-box">
+                <input type="email" placeholder="Email Adresinizi Giriniz..." required/>
+                <div class="underline"></div>
+            </div>
+
+            <div class="input-box">
+                <input type="email" placeholder="Şifrenizi Giriniz..." required/>
+                <div class="underline"></div>
+            </div>
+
+
+            <div class="input-box button">
+                <input type="submit" value="Kayıt Ol"/>
+            </div>
+            
+
+        </form>
+
+        <div class="text">Diğer Seçeneklerle Kayıt Olun</div>
+
+
+        <div class="twitter">
+            <a href="#"><i class="fab fa-twitter"></i>Twitter ile Giriş</a>
+        </div>
+
+        <div class="facebook">
+            <a href="#"><i class="fab fa-facebook"></i>Facebook ile Giriş</a>
+        </div>
+
+
     </div>
   );
 }
